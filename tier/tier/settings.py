c0716165cc52dcbcf551882e7e794e2992258386
mugdhakolte <koltemugdha@gmail.com>
2022-06-09 17:30:42 +0200

    feat: added docker configuration

--- tier/tier/settings.py
+++ tier/tier/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,8 +85,19 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tier',
         'USER': 'postgres',
-        'PASSWORD': 'password'
+        'PASSWORD': 'password',
+        # 'HOST': 'localhost',
+        'HOST': 'db',
+        'PORT': 5432
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get('POSTGRES_NAME'),
+    #     'USER': os.environ.get('POSTGRES_USER'),
+    #     'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+    #     'HOST': 'db',
+    #     'PORT': 5432,
+    # }
 }
 
 
